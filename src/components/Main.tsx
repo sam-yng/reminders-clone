3fcf1bd3e179cf45892e5ebb0b515a47c56dc000
sam-yng <young142001@hotmail.com>
2023-04-07 10:12:15 +1000

    pre easter push

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -82,8 +82,6 @@ const Main: React.FC = () => {
     }
   };
 
-  const dateValue = document.querySelector('input[type="date');
-
   return (
     <main className="w-[65%] ml-16 mt-8">
       <article className="flex flex-row">
@@ -130,6 +128,7 @@ const Main: React.FC = () => {
               <button className="ml-auto mr-6 w-36 flex p-2 border-2 border-slate-100 rounded-lg items-cente bg-slate-100">
                 <img className="h-4 pl-2" src={calendar} />
                 <input
+                  name="date"
                   className="bg-slate-100 w-[70%] ml-2 text-center text-[13px] focus:outline-none"
                   type="text"
                   placeholder="Add Date"
@@ -137,9 +136,25 @@ const Main: React.FC = () => {
               </button>
 
               <button
-                onClick={() => {
-                  item.flagged = !item.flagged;
-                }}
+                onClick={() =>
+                  setLists(
+                    lists.map((list) =>
+                      list.id === activeListId
+                        ? {
+                            ...activeList,
+                            tasks: activeList.tasks.map((task) =>
+                              task.id === item.id
+                                ? {
+                                    ...item,
+                                    flagged: !item.flagged,
+                                  }
+                                : task
+                            ),
+                          }
+                        : list
+                    )
+                  )
+                }
                 className="p-2 rounded-lg border-2 bg-slate-100 border-slate-100"
               >
                 <img
@@ -165,9 +180,25 @@ const Main: React.FC = () => {
                 </button>
 
                 <button
-                  onClick={() => {
-                    item.flagged = !item.flagged;
-                  }}
+                  onClick={() =>
+                    setLists(
+                      lists.map((list) =>
+                        list.id === activeListId
+                          ? {
+                              ...activeList,
+                              tasks: activeList.tasks.map((task) =>
+                                task.id === item.id
+                                  ? {
+                                      ...item,
+                                      flagged: !item.flagged,
+                                    }
+                                  : task
+                              ),
+                            }
+                          : list
+                      )
+                    )
+                  }
                   className="p-2 rounded-lg border-2 bg-slate-100 border-slate-100"
                 >
                   <img
@@ -194,9 +225,25 @@ const Main: React.FC = () => {
                 </button>
 
                 <button
-                  onClick={() => {
-                    item.flagged = !item.flagged;
-                  }}
+                  onClick={() =>
+                    setLists(
+                      lists.map((list) =>
+                        list.id === activeListId
+                          ? {
+                              ...activeList,
+                              tasks: activeList.tasks.map((task) =>
+                                task.id === item.id
+                                  ? {
+                                      ...item,
+                                      flagged: !item.flagged,
+                                    }
+                                  : task
+                              ),
+                            }
+                          : list
+                      )
+                    )
+                  }
                   className="p-2 rounded-lg border-2 bg-slate-100 border-slate-100"
                 >
                   <img
